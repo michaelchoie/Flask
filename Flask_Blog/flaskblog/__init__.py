acb36abb13a87fcaa0dcec1b0e4ceaee1cf6c250
Michael Choie <MichaelChoie@Michaels-MacBook-Pro-2.local>
2018-07-25 11:36:46 -0400

    First commit

---
+++ Flask_Blog/flaskblog/__init__.py
@@ -0,0 +1,10 @@
+import secrets
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] =
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+from flaskapp import routes
---
+++ Model_Deploy/webapp/routes.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+import os
+from flask import render_template, request
+from keras.models import load_model
+from webapp import app
+
+# Route tells Flask what URL should trigger the function
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    # request object gives you access to parsed request data
+    if request.method == "POST":
+        file = request.files['image']
+        if not file:
+            return render_template("home.html", label="No file")
+
+        # Read in the model
+        model = load_model(os.path.abspath("mnist.model.best.hdf5"))
+
+        # Image file read in as file storage object - must preprocess
+        filestr = file.read()
+        npimg = np.fromstring(filestr, np.uint8)
+        img = cv2.imdecode(npimg, cv2.IMREAD_GRAYSCALE)
+        img = img.astype("float32") / 255
+        img = img.reshape(1, 28, 28)
+
+        # Predict class and render web page with the value
+        label = model.predict_classes(img)
+
+        return render_template("predict.html", label=label)
